beac85ab97d96a991f0facd20bb8ebe00f513087
5chang2 <och8808@gmail.com>
2020-11-05 14:10:36 +0900

    update

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import ArticleSerializer, ArticleListSerializer
+from .models import Article
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def list_and_create(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+    else:
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+    return Response(serializer.data)
